0b403868383ac7ab7cd494397d3b4262d533546f
atangeman <andytangeman@gmail.com>
2017-02-28 08:23:54 +0000

    Reference library in program driver, rebuild and recompile shared assemblies

--- ProcessLibrary/Helpers/ProcessEventTypes.cs
+++ ProcessLibrary/Helpers/ProcessEventTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BatchProcessDriver.Helpers
+﻿namespace ProcessLibrary.Helpers
 {
     /// <summary>
     /// Enumtype with levels of notification for assigned process arguments.
