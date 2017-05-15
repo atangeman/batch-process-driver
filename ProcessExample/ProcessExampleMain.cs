ca4f9e0eb345de71c85d4c211a217d5394e756c0
atangeman <andytangeman@gmail.com>
2017-05-15 13:32:18 -0700

    added oricess exanoke

---
+++ ProcessExample/ProcessExampleMain.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ProcessExample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Example process to demonstrate functionality of application
+    /// </summary>
+    public class ProcessExampleMain
+    {
+    }
+}
