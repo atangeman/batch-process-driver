58f2471ef8337c8d57d5e5dc8f8da57d3412dcad
atangeman <andytangeman@gmail.com>
2017-05-15 13:11:12 -0700

    Added IProcessArgs.cs

---
+++ ProcessLibrary/Events/IProcessArgs.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="IProcessArgs.cs" company="City of San Diego">
+// Copyright (c) City of San Diego. All rights reserved.
+// </copyright>
+
+namespace FireJobUtilities.Events
+{
+    /// <summary>
+    /// Generic interface for passing arguments to Process event handlers.
+    /// </summary>
+    public interface IProcessArgs
+    {
+        /// <summary>
+        /// Gets string message to pass to event handler / delegate.
+        /// </summary>
+        string Message { get; }
+    }
+}
